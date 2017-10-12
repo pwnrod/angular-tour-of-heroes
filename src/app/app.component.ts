800de94d6fe0a5edde4d473f6c9f24e047381565
Caylin James <caylin@lougeek.com>
2017-10-12 08:08:06 -0400

    Part 5 complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,7 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
-export class Hero {
-    id: number;
-    name: string;
-}
-
-const HEROES: Hero[] = [
-    {id: 11, name: 'Mr. Rice'},
-    {id: 12, name: 'Oscillator Terminator'},
-    {id: 13, name: 'Bonbeasto'},
-    {id: 14, name: 'CeleryGuy'},
-    {id: 15, name: 'MagnetHands'},
-    {id: 16, name: 'RubberFace'},
-    {id: 17, name: 'Dyn-Dyn-DWAN'},
-    {id: 18, name: 'Dr. Doctor Dock Turr'},
-    {id: 19, name: 'MagdelinaMagnifique'},
-    {id: 20, name: 'WindyGuy'}
-];
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-app',
@@ -24,18 +9,77 @@ const HEROES: Hero[] = [
         <h1>{{title}}</h1>
         <h2>My Heroes</h2>
         <ul class="heroes">
-            <li *ngFor="let hero of heroes">
+            <li *ngFor="let hero of heroes" 
+                [class.selected]="hero === selectedHero"
+                (click)="onSelect(hero)"> 
                 <span class="badge">{{hero.id}}</span> {{hero.name}}
             </li>
         </ul>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
-    `
+        <hero-detail [hero]="selectedHero"></hero-detail>
+    `,
+    styles: [`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .heroes {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 15em;
+        }
+        .heroes li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .heroes li.selected:hover {
+            background-color: #BBD8DC !important;
+            color: white;
+        }
+        .heroes li:hover {
+            color: #607D8B;
+            background-color: #DDD;
+            left: .1em;
+        }
+        .heroes .text {
+            position: relative;
+            top: -3px;
+        }
+        .heroes .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0 0 4px;
+        }
+    `],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
-    heroes = HEROES;
+    heroes: Hero[];
+    selectedHero: Hero;
+    constructor(private heroService: HeroService) { }
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    };
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Rice'},
+    {id: 12, name: 'Oscillator Terminator'},
+    {id: 13, name: 'Bonbeasto'},
+    {id: 14, name: 'CeleryGuy'},
+    {id: 15, name: 'MagnetHands'},
+    {id: 16, name: 'RubberFace'},
+    {id: 17, name: 'Dyn-Dyn-DWAN'},
+    {id: 18, name: 'Dr. Doctor Dock Turr'},
+    {id: 19, name: 'MagdelinaMagnifique'},
+    {id: 20, name: 'WindyGuy'}
+];
