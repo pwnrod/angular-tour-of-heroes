84bd706343162a00c7a986612a00330812b9de82
Caylin James <caylin@lougeek.com>
2017-10-16 08:49:43 -0400

    Part 7 finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,20 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
-import {DashboardComponent} from "./dashboard.component";
+import {DashboardComponent} from './dashboard.component';
+
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
@@ -35,9 +22,7 @@ import {DashboardComponent} from "./dashboard.component";
         HeroesComponent,
         DashboardComponent
     ],
-    providers: [
-        HeroService
-    ],
+    providers: [HeroService],
     bootstrap: [AppComponent],
 })
 export class AppModule {
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,5 +7,9 @@ import { HEROES } from './mock-heroes';
 export class HeroService {
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
-    } // stub
+    }
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
 }
